2dba212881dd53a09db35adedbde890aa9a441b6
Wordhater <ryanlocke058@gmail.com>
2024-11-24 16:04:56 +1000

    release V0.1

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 use gtk::{glib::Char, Label};
 use gtk::{prelude::*, Separator};
 use gio::{glib::clone, prelude::*};
+use std::collections::linked_list::Iter;
 use std::{collections::LinkedList};
 use adw::Application;
-use gtk::{glib, Application as GTKApplication, ApplicationWindow, Button, SpinButton, Box};
+use gtk::{glib, ApplicationWindow, Button, SpinButton, Box};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 const APP_ID: &str = "org.gtk_rs.WCM_UI";
-
 
 // other functions
 
 fn get_item_by_index_str(list: &LinkedList<String>, index: usize) -> String {
-    let mut iter = list.iter();
+    let mut iter: Iter<String> = list.iter();
     for _ in 0..index {
         if iter.next().is_none() {
             return "Index out of bounds".to_string();
         }
     }
-    iter.next().map(|s| s.clone()).unwrap_or_else(|| "Index out of bounds".to_string())
+    iter.next().map(|s: &String| s.clone()).unwrap_or_else(|| "Index out of bounds".to_string())
 }
 
 fn count_words(input: &str) -> i32 {
-    let parts = input.split(" ");
+    let parts: std::str::Split<'_, &str> = input.split(" ");
     let mut count: i32 = 0;
     for part in parts{
         count += 1;
@@ -39,12 +41,12 @@ fn is_all_numeric(s: &str) -> bool {
 // decrease
 fn decrease<'a>(input: &'a str, goal: i32, replacement: &'a str) -> String{
     let init_count: i32 = count_words(input);
-    let parts = input.split(" ");
+    let parts: std::str::Split<'_, &str> = input.split(" ");
     let mut words:LinkedList<String> = LinkedList::new();
     for part in parts{
         words.push_back(part.to_string());
     }
-    let mut output: String = get_item_by_index_str(&words, 0 as usize);
+    let mut output: String = "".to_string();
     let diff: i32 = words.len() as i32 - goal;
     if goal >= init_count/2 {
         let rate: f32 = (words.len() as f32 -1.0)/diff as f32;
@@ -91,7 +93,6 @@ fn modifywrapper<'a>(input: &'a str, count: i32, replacement: &'a String) -> Str
 }
 
 fn main()  -> glib::ExitCode {
-    const TESTING: bool = true;
     // Setup chars
     let mut addCHARS:LinkedList<String> = LinkedList::new();
     addCHARS.push_back("\u{2000}".to_string());
@@ -99,14 +100,6 @@ fn main()  -> glib::ExitCode {
     addCHARS.push_back("\u{205f}".to_string());//best
     addCHARS.push_back("\u{2004}".to_string());//potential inconsistency
     addCHARS.push_back("TEST".to_string());//testing
-
-    let selected: String = get_item_by_index_str(&addCHARS, 2);
-    if TESTING{
-        println!("{}", count_words("1 2 3 4 5 6 7 8 9 10"));
-        let out: &str = &modifywrapper("df k j h f df ds h f k j j a l k s f h a s ld k ja k j sl fh l s k a f h a s j f h a s d k j f h a s k d h f k a s d fasdh fasdjhf jhsdfk ljsah as fk lj sa hfk asdf kh asd fkja shd fasdkjhfasdk ", 40, &selected);
-        println!("result: {}", count_words(out));
-        println!("text: {}", out);
-    }
     // Create a new application
 
     // bootGUI();
@@ -121,6 +114,7 @@ fn main()  -> glib::ExitCode {
 }
 
 fn bootGUI(app: &Application){
+
     let gtk_box = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .build();
@@ -185,15 +179,48 @@ fn bootGUI(app: &Application){
         .use_markup(true)
         .build();
     output_title.set_markup("<span font=\"15\"><b> </b></span>");
+
+    // mode selection
+
+
+    // char buttons
+    let char_label =  Label::builder()
+        .label("Char selection (SELECT ONLY ONE AT A TIME): ")
+        .build();
+    let char_btn_0 = gtk::ToggleButton::builder()
+        .label("U+205F")
+        .build();
+
+    let char_btn_1 = gtk::ToggleButton::builder()
+        .label("U+2004")
+        .build();
+
+    let char_btn_2 = gtk::ToggleButton::builder()
+        .label("_")
+        // .group("yee")
+        .build();
+
+    let charbuttons = Rc::new(RefCell::new(vec![char_btn_0.clone(), char_btn_1.clone(), char_btn_2.clone()]));
+    char_btn_0.set_active(true);
     // layouts
-    let row1 = Box::new(gtk::Orientation::Horizontal, 5);
+    let row1: Box = Box::new(gtk::Orientation::Horizontal, 5);
     row1.set_margin_top(5);
     row1.set_margin_bottom(5);
+
+    let charrow: Box = Box::new(gtk::Orientation::Horizontal, 5);
+    charrow.set_margin_top(5);
+    charrow.set_margin_bottom(5);
+
+    charrow.append(&char_label);
+    charrow.append(&char_btn_0);
+    charrow.append(&char_btn_1);
+    charrow.append(&char_btn_2);
 
     row1.append(&count_input_label);
     row1.append(&count_input);
 
     gtk_box.append(&title);
+    gtk_box.append(&charrow);
     gtk_box.append(&input_text);
     gtk_box.append(&horizontal_separator_0);
     gtk_box.append(&word_count);
@@ -206,22 +233,44 @@ fn bootGUI(app: &Application){
     gtk_box.append(&output);
 
     // Interaction
-    input_text.connect_changed(move |_text| {
+    input_text.connect_changed(move |_text: &gtk::Text| {
         if _text.text_length() == 0 {word_count.set_label("Current Word Count: 0");}else{
             let mut lable: String = "Current Word Count: ".to_string();
             lable.push_str(&count_words(&_text.text()).to_string());
+
             word_count.set_label(&lable);
         }
     });
 
-    count_input.connect_changed(|_count_input| {
-        println!("update")
+    // jank method of ensuring count input is a number
+    count_input.connect_changed(|_count_input: &gtk::Entry| {
+        println!("update");
+        let mut text: &str = &_count_input.text().to_string();
+        if text.chars().all(char::is_numeric){
+            println!("all numbers")
+        }else{
+            println!("letters detected");
+            let mut output: String = text.to_string();
+            output.pop();
+            println!("{}", output);
+            _count_input.set_text(&output);
+        }
     });
+    // toggle controller - this never worked
+    // for button in charbuttons.borrow().iter() {
+    //     let charbuttons: Rc<RefCell<Vec<gtk::ToggleButton>>> = Rc::clone(&charbuttons);
+    //     button.connect_toggled(move |btn| {
+    //         if btn.is_active() {
+    //             // Deactivate all other buttons when this one is activated
+    //             for other_button in charbuttons.borrow().iter() {
+    //                 other_button.set_active(false);
+    //             }
+    //             btn.activate();
+    //         } else{println!("deactivate")}
+    //     });
+    // }
 
-
-    
-
-    let main_window = gtk::ApplicationWindow::builder()
+    let main_window: ApplicationWindow = gtk::ApplicationWindow::builder()
         .application(app)
         .default_width(600)
         .default_height(700)
@@ -232,13 +281,33 @@ fn bootGUI(app: &Application){
     apply_button.connect_clicked(move |_button| {
         println!("modify btn clicked");
         if count_input.text().parse::<i32>().unwrap() as i32 == 0{println!("no number input")}else{
-            let result = &modifywrapper(&input_text.text().to_string(), count_input.text().parse::<i32>().unwrap() as i32, &"\u{2004}".to_string());
-            let markup = format!("{}", result);
+            let mut char = "".to_string();
+            // get selected char
+            for button in charbuttons.borrow().iter() {
+                if button.is_active(){
+                    let label: &str = match button.label() {
+                        Some(formatted) => &formatted.clone(),
+                        None => "null",
+                    };
+                    // add extra chars here
+                    if label == "U+205F"{
+                        char = "\u{205f}".to_string();
+                        break
+                    }
+                    if label == "U+2004"{
+                        char = "\u{2004}".to_string();
+                        break
+                    }
+                    if label == "_"{
+                        char = "_".to_string();
+                        break
+                    }
+                }
+            }
+            let result: &String = &modifywrapper(&input_text.text().to_string(), count_input.text().parse::<i32>().unwrap() as i32, &char);
+            let markup: String = format!("{}", result);
             output.buffer().set_text(&markup);
             output_title.set_markup(&format!("<span font=\"15\"><b>Result: {} words</b></span>", count_words(result)));
-
-            // output.set_markup(&markup);
-            // gtk_box.append(&output);
         }
     });
     main_window.present();
